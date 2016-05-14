b8a19ecebee12e115f474ba5bef78926844360d1
vipin yadav <vkyadav998@gmail.com>
2016-05-14 19:20:48 +0530

    body parser

--- app3.js
+++ app3.js
@@ -2,7 +2,11 @@
  * Created by pc on 14-05-2016.
  */
 var express = require("express");
+var bodyParser= require("body-parser");
 var app=new express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/vk",function (req,res) {
     console.log(req.query);
@@ -16,6 +20,13 @@ app.post("/vk",function (req,res) {
     var name=req.query.name;
     var mname=req.query.mname;
     var lname=req.query.lname;
+    res.send("Post my name is "+ name+" "+mname+" "+lname);
+});
+
+app.post("/vky",function (req,res) {
+    var name=req.body.name;
+    var mname=req.body.mname;
+    var lname=req.body.lname;
     res.send("Post my name is "+ name+" "+mname+" "+lname);
 });
 
